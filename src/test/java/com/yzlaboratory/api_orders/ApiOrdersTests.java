a2587ca4039ea8f4100032fa72affd43b93bb62f
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-11-18 12:21:32 +0100

    declared db classes as mocks

--- src/test/java/com/yzlaboratory/api_orders/ApiOrdersTests.java
+++ src/test/java/com/yzlaboratory/api_orders/ApiOrdersTests.java
@@ -1,10 +1,19 @@
 package com.yzlaboratory.api_orders;
 
+import com.yzlaboratory.api_orders.repository.OrdersRepository;
+import com.yzlaboratory.api_orders.service.OrdersService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ApiOrdersTests {
+
+	@MockitoBean
+	OrdersService service;
+
+	@MockitoBean
+	OrdersRepository repository;
 
 	@Test
 	void contextLoads() {
